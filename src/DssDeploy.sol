d869179632e2d9a43fdfaef9831c6dc25b0a487c
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2018-08-24 17:03:13 -0300

    Upgrade dss repo: drip, auth and others

--- src/DssDeploy.sol
+++ src/DssDeploy.sol
@@ -1,107 +1,126 @@
 pragma solidity ^0.4.24;
 
+import {DSToken} from "ds-token/token.sol";
+import {DSAuth, DSAuthority} from "ds-auth/auth.sol";
+
 import {Vat} from "dss/tune.sol";
 import {Pit} from "dss/frob.sol";
-import {Dai20} from "dss/transferFrom.sol";
 import {Drip} from "dss/drip.sol";
 import {Vow} from "dss/heal.sol";
 import {Cat} from "dss/bite.sol";
+import {DaiAdapter} from "dss/join.sol";
 import {Flapper} from "dss/flap.sol";
 import {Flopper} from "dss/flop.sol";
 import {Flipper} from "dss/flip.sol";
 
 import {Price} from "./poke.sol";
+import {DaiMom} from "./mom.sol";
 
 contract VatFab {
     function newVat() public returns (Vat vat) {
         vat = new Vat();
-        // vat.setOwner(msg.sender);
+        vat.rely(msg.sender);
     }
 }
 
 contract PitFab {
     function newPit(Vat vat) public returns (Pit pit) {
         pit = new Pit(vat);
-        // pit.setOwner(msg.sender);
-    }
-}
-
-contract PieFab {
-    function newPie(Vat vat) public returns (Dai20 pie) {
-        pie = new Dai20(vat);
-        // pie.setOwner(msg.sender);
+        pit.rely(msg.sender);
     }
 }
 
 contract DripFab {
     function newDrip(Vat vat) public returns (Drip drip) {
         drip = new Drip(vat);
-        // drip.setOwner(msg.sender);
+        drip.rely(msg.sender);
     }
 }
 
 contract VowFab {
     function newVow() public returns (Vow vow) {
         vow = new Vow();
-        // vow.setOwner(msg.sender);
+        vow.rely(msg.sender);
     }
 }
 
 contract CatFab {
     function newCat(Vat vat, Pit pit, Vow vow) public returns (Cat cat) {
         cat = new Cat(vat, pit, vow);
-        // cat.setOwner(msg.sender);
+        cat.rely(msg.sender);
+    }
+}
+
+contract TokenFab {
+    function newToken(bytes32 symbol) public returns (DSToken token) {
+        token = new DSToken(symbol);
+        token.setOwner(msg.sender);
+    }
+}
+
+contract DaiAptFab {
+    function newDaiApt(Vat vat, address dai) public returns (DaiAdapter daiApt) {
+        daiApt = new DaiAdapter(vat, dai);
     }
 }
 
 contract FlapFab {
-    function newFlap(Dai20 pie, address gov) public returns (Flapper flap) {
-        flap = new Flapper(pie, gov);
-        // flap.setOwner(msg.sender);
+    function newFlap(address dai, address gov) public returns (Flapper flap) {
+        flap = new Flapper(dai, gov);
     }
 }
 
 contract FlopFab {
-    function newFlop(Dai20 pie, address gov) public returns (Flopper flop) {
-        flop = new Flopper(pie, gov);
-        // flop.setOwner(msg.sender);
+    function newFlop(address dai, address gov) public returns (Flopper flop) {
+        flop = new Flopper(dai, gov);
+        flop.rely(msg.sender);
     }
 }
 
 contract FlipFab {
     function newFlip(Vat vat, bytes32 ilk) public returns (Flipper flop) {
         flop = new Flipper(vat, ilk);
-        // flip.setOwner(msg.sender);
     }
 }
 
 contract PriceFab {
     function newPrice(Pit pit, bytes32 ilk) public returns (Price price) {
         price = new Price(pit, ilk);
-        // price.setOwner(msg.sender);
+        price.rely(msg.sender);
     }
 }
 
-contract DssDeploy /*is DSAuth*/ {
+contract MomFab {
+    function newMom() public returns (DaiMom mom) {
+        mom = new DaiMom();
+        mom.setOwner(msg.sender);
+    }
+}
+
+contract DssDeploy is DSAuth {
     VatFab public vatFab;
     PitFab public pitFab;
-    PieFab public pieFab;
     DripFab public dripFab;
     VowFab public vowFab;
     CatFab public catFab;
+    TokenFab public tokenFab;
+    DaiAptFab public daiAptFab;
     FlapFab public flapFab;
     FlopFab public flopFab;
+    MomFab public momFab;
     FlipFab public flipFab;
     PriceFab public priceFab;
 
     Vat public vat;
     Pit public pit;
-    Dai20 public pie;
     Drip public drip;
     Vow public vow;
     Cat public cat;
+    DSToken public dai;
+    DaiAdapter public daiApt;
     Flapper public flap;
     Flopper public flop;
+    DaiMom public mom;
     mapping(bytes32 => Ilk) public ilks;
 
     uint8 public step = 0;
@@ -114,62 +133,108 @@ contract DssDeploy /*is DSAuth*/ {
         Price price;
     }
 
-    constructor(VatFab vatFab_, PitFab pitFab_, PieFab pieFab_, DripFab dripFab_, VowFab vowFab_, CatFab catFab_, FlapFab flapFab_, FlopFab flopFab_, FlipFab flipFab_, PriceFab priceFab_) public {
+    constructor(
+        VatFab vatFab_,
+        PitFab pitFab_,
+        DripFab dripFab_,
+        VowFab vowFab_,
+        CatFab catFab_,
+        TokenFab tokenFab_,
+        DaiAptFab daiAptFab_,
+        FlapFab flapFab_,
+        FlopFab flopFab_,
+        MomFab momFab_,
+        FlipFab flipFab_,
+        PriceFab priceFab_
+    ) public {
         vatFab = vatFab_;
         pitFab = pitFab_;
-        pieFab = pieFab_;
         dripFab = dripFab_;
         vowFab = vowFab_;
         catFab = catFab_;
+        tokenFab = tokenFab_;
+        daiAptFab = daiAptFab_;
         flapFab = flapFab_;
         flopFab = flopFab_;
+        momFab = momFab_;
         flipFab = flipFab_;
         priceFab = priceFab_;
     }
 
-    function deployContracts(address gov) public /*auth */{
+    function deployContracts(address gov) public auth {
         require(step == 0);
         vat = vatFab.newVat();
         pit = pitFab.newPit(vat);
-        pie = pieFab.newPie(vat);
         drip = dripFab.newDrip(vat);
         vow = vowFab.newVow();
         cat = catFab.newCat(vat, pit, vow);
-        flap = flapFab.newFlap(pie, gov);
-        flop = flopFab.newFlop(pie, gov);
+        dai = tokenFab.newToken("DAI");
+        daiApt = daiAptFab.newDaiApt(vat, dai);
+        flap = flapFab.newFlap(daiApt, gov); // TODO: Check if pass adapter or token
+        flop = flopFab.newFlop(daiApt, gov); // TODO: Check if pass adapter or token
+        mom = momFab.newMom();
 
         vow.file("vat", vat);
         vow.file("flap", flap);
         vow.file("flop", flop);
+        pit.file("drip", drip);
+
+        // Internal auth
+        vat.rely(drip);
+        vat.rely(pit);
+        vat.rely(cat);
+        vat.rely(daiApt);
+        vat.rely(flap);
+        vat.rely(flop);
+        vow.rely(cat);
+        flop.rely(vow);
+
         step += 1;
     }
 
-    function deployIlk(bytes32 ilk, address adapter, address pip) public /*auth */{
+    function deployIlk(bytes32 ilk, address adapter, address pip) public auth {
         require(step > 0);
         ilks[ilk].flip = flipFab.newFlip(vat, ilk);
         ilks[ilk].adapter = adapter;
         ilks[ilk].price = priceFab.newPrice(pit, ilk);
-        ilks[ilk].price.setPip(pip);
-        ilks[ilk].price.setMat(ONE);
-        ilks[ilk].price.poke();
+        ilks[ilk].price.file(pip); // Set pip
+        ilks[ilk].price.file(ONE); // Set mat
         cat.fuss(ilk, ilks[ilk].flip);
         cat.file(ilk, "chop", ONE);
         vat.init(ilk);
-        // drip.file(ilk, bytes32(address(vow)), ONE);
+        drip.file(ilk, bytes32(address(vow)), ONE);
+
+        // Internal auth
+        vat.rely(ilks[ilk].flip);
+        vat.rely(adapter);
+        pit.rely(ilks[ilk].price);
+        ilks[ilk].price.rely(mom);
+
+        ilks[ilk].price.poke();
     }
 
-    function configParams() public /*auth */{
+    function configParams() public auth {
         require(step == 1);
         step += 1;
     }
 
-    function verifyParams() public /*auth */{
+    function verifyParams() public auth {
         require(step == 2);
         step += 1;
     }
 
-    function configAuth(/*DSAuthority authority*/) public /*auth */{
+    function configAuth(DSAuthority authority) public auth {
         require(step == 3);
+        pit.rely(mom);
+        cat.rely(mom);
+        vow.rely(mom);
+        drip.rely(mom);
+
+        mom.setAuthority(authority);
+        mom.setOwner(0);
+        this.setAuthority(authority);
+        this.setOwner(0);
+
         step += 1;
     }
 }
--- src/DssDeploy.t.sol
+++ src/DssDeploy.t.sol
@@ -2,22 +2,23 @@ pragma solidity ^0.4.24;
 
 import {DSTest} from "ds-test/test.sol";
 import {DSValue} from "ds-value/value.sol";
-import {DSToken} from "ds-token/token.sol";
+import {DSRoles} from "ds-roles/roles.sol";
 
-import {Adapter} from "dss/join.sol";
-import {AdapterETH} from "./join.sol";
+import {Adapter, ETHAdapter} from "dss/join.sol";
 
 import "./DssDeploy.sol";
 
 contract DssDeployTest is DSTest {
     VatFab vatFab;
     PitFab pitFab;
-    PieFab pieFab;
     DripFab dripFab;
     VowFab vowFab;
     CatFab catFab;
+    TokenFab tokenFab;
+    DaiAptFab daiAptFab;
     FlapFab flapFab;
     FlopFab flopFab;
+    MomFab momFab;
     FlipFab flipFab;
     PriceFab priceFab;
 
@@ -27,30 +28,33 @@ contract DssDeployTest is DSTest {
     DSValue pipETH;
     DSValue pipDGX;
 
-    // DSRoles authority;
+    DSRoles authority;
 
     function setUp() public {
         vatFab = new VatFab();
         pitFab = new PitFab();
-        pieFab = new PieFab();
         dripFab = new DripFab();
         vowFab = new VowFab();
         catFab = new CatFab();
+        tokenFab = new TokenFab();
+        daiAptFab = new DaiAptFab();
         flapFab = new FlapFab();
         flopFab = new FlopFab();
+        momFab = new MomFab();
+
         flipFab = new FlipFab();
         priceFab = new PriceFab();
 
         uint startGas = gasleft();
-        dssDeploy = new DssDeploy(vatFab, pitFab, pieFab, dripFab, vowFab, catFab, flapFab, flopFab, flipFab, priceFab);
+        dssDeploy = new DssDeploy(vatFab, pitFab, dripFab, vowFab, catFab, tokenFab, daiAptFab, flapFab, flopFab, momFab, flipFab, priceFab);
         uint endGas = gasleft();
         emit log_named_uint("Deploy DssDeploy", startGas - endGas);
 
         gov = new DSToken("GOV");
         pipETH = new DSValue();
         pipDGX = new DSValue();
-        // authority = new DSRoles();
-        // authority.setRootUser(this, true);
+        authority = new DSRoles();
+        authority.setRootUser(this, true);
     }
 
     function testDeploy() public {
@@ -59,8 +63,8 @@ contract DssDeployTest is DSTest {
         uint endGas = gasleft();
 
         startGas = gasleft();
-        AdapterETH adapterETH = new AdapterETH(dssDeploy.vat(), "ETH");
-        dssDeploy.deployIlk("ETH", adapterETH, pipETH);
+        ETHAdapter ethAdapter = new ETHAdapter(dssDeploy.vat(), "ETH");
+        dssDeploy.deployIlk("ETH", ethAdapter, pipETH);
         endGas = gasleft();
         emit log_named_uint("Make Vox Tub", startGas - endGas);
 
@@ -82,9 +86,12 @@ contract DssDeployTest is DSTest {
         emit log_named_uint("Verify Params", startGas - endGas);
 
         startGas = gasleft();
-        dssDeploy.configAuth(/*authority*/);
+        dssDeploy.configAuth(authority);
         endGas = gasleft();
         emit log_named_uint("Config Auth", startGas - endGas);
+
+        ethAdapter.join.value(10)();
+        assertEq(dssDeploy.vat().gem("ETH", bytes32(address(this))), 10);
     }
 
     function testFailStep() public {

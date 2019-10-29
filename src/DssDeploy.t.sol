b84633577b0ec4d9f65139225c3f3b5a44037c1a
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2019-10-29 17:23:24 -0300

    Allow to pass address(0) as pip when deploying a collateral

--- src/DssDeploy.t.sol
+++ src/DssDeploy.t.sol
@@ -735,13 +735,12 @@ contract DssDeployTest is DssDeployTestBase {
 
     function testFailJoinAfterCageAuthGemJoin() public {
         deployKeepAuth();
-        DSValue pip = new DSValue();
 
         DSToken sai = new DSToken("SAI");
         sai.mint(20);
         AuthGemJoin saiJoin = new AuthGemJoin(address(vat), "SAI", address(sai));
 
-        dssDeploy.deployCollateral("SAI", address(saiJoin), address(pip));
+        dssDeploy.deployCollateral("SAI", address(saiJoin), address(0));
 
         sai.approve(address(saiJoin), uint(-1));
         saiJoin.join(address(this), 10);
@@ -751,14 +750,13 @@ contract DssDeployTest is DssDeployTestBase {
 
     function testTokenSai() public {
         deployKeepAuth();
-        DSValue pip = new DSValue();
 
         DSToken sai = new DSToken("SAI");
         sai.mint(10);
         AuthGemJoin saiJoin = new AuthGemJoin(address(vat), "SAI", address(sai));
         assertEq(saiJoin.dec(), 18);
 
-        dssDeploy.deployCollateral("SAI", address(saiJoin), address(pip));
+        dssDeploy.deployCollateral("SAI", address(saiJoin), address(0));
 
         sai.approve(address(saiJoin), uint(-1));
         assertEq(sai.balanceOf(address(saiJoin)), 0);
@@ -774,13 +772,12 @@ contract DssDeployTest is DssDeployTestBase {
 
     function testFailTokenSai() public {
         deployKeepAuth();
-        DSValue pip = new DSValue();
 
         DSToken sai = new DSToken("SAI");
         sai.mint(10);
         AuthGemJoin saiJoin = new AuthGemJoin(address(vat), "SAI", address(sai));
 
-        dssDeploy.deployCollateral("SAI", address(saiJoin), address(pip));
+        dssDeploy.deployCollateral("SAI", address(saiJoin), address(0));
 
         sai.approve(address(saiJoin), uint(-1));
         saiJoin.deny(address(this));

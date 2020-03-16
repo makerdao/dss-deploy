68be3125dc8e6c1ea6cfff532b59951add6b8216
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-03-16 11:30:24 -0300

    Add GemJoin5

--- src/join.sol
+++ src/join.sol
@@ -262,6 +262,62 @@ contract GemJoin4 is LibNote {
     }
 }
 
+// GemJoin5
+// For a token that has a lower precision than 18 and it has decimals (like USDC)
+
+contract GemLike5 {
+    function decimals() public view returns (uint8);
+    function transfer(address,uint) public returns (bool);
+    function transferFrom(address,address,uint) public returns (bool);
+}
+
+contract GemJoin5 is LibNote {
+    // --- Auth ---
+    mapping (address => uint) public wards;
+    function rely(address usr) external note auth { wards[usr] = 1; }
+    function deny(address usr) external note auth { wards[usr] = 0; }
+    modifier auth { require(wards[msg.sender] == 1); _; }
+
+    VatLike  public vat;
+    bytes32  public ilk;
+    GemLike5 public gem;
+    uint     public dec;
+    uint     public live;  // Access Flag
+
+    constructor(address vat_, bytes32 ilk_, address gem_) public {
+        dec = gem.decimals();
+        require(dec < 18, "GemJoin3/decimals-18-or-higher");
+        wards[msg.sender] = 1;
+        live = 1;
+        vat = VatLike(vat_);
+        ilk = ilk_;
+        gem = GemLike5(gem_);
+    }
+
+    function cage() external note auth {
+        live = 0;
+    }
+
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, "GemJoin3/overflow");
+    }
+
+    function join(address urn, uint wad) public note {
+        require(live == 1, "GemJoin3/not-live");
+        uint wad18 = mul(wad, 10 ** (18 - dec));
+        require(int(wad18) >= 0, "GemJoin3/overflow");
+        vat.slip(ilk, urn, int(wad18));
+        require(gem.transferFrom(msg.sender, address(this), wad), "GemJoin3/failed-transfer");
+    }
+
+    function exit(address guy, uint wad) public note {
+        uint wad18 = mul(wad, 10 ** (18 - dec));
+        require(int(wad18) >= 0, "GemJoin3/overflow");
+        vat.slip(ilk, msg.sender, -int(wad18));
+        require(gem.transfer(guy, wad), "GemJoin3/failed-transfer");
+    }
+}
+
 // AuthGemJoin
 // For a token that needs restriction on the sources which are able to execute the join function (like SAI through Migration contract)
 

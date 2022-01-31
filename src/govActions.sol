bbfe3538f76a14909e311427912fdd891b977442
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-01-31 11:04:32 -0500

    docs: update copyright

--- src/govActions.sol
+++ src/govActions.sol
@@ -2,8 +2,7 @@
 //
 // govActions.sol
 //
-// Copyright (C) 2018-2021 Maker Ecosystem Growth Holdings, INC.
-// Copyright (C) 2021-2022 Dai Foundation
+// Copyright (C) 2018-2022 Dai Foundation
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by

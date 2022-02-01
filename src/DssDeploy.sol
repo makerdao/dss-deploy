c40f77110247cbe0afba3c1ff2018793f45a1878
Julien <80432849+julienmartinlevrai@users.noreply.github.com>
2022-01-31 22:51:47 -0500

    docs: update copyright (#66)

--- src/DssDeploy.sol
+++ src/DssDeploy.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-
-/// DssDeploy.sol
-
-// Copyright (C) 2018-2020 Maker Ecosystem Growth Holdings, INC.
-
+//
+// DssDeploy.sol
+//
+// Copyright (C) 2018-2022 Dai Foundation
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
